ae14555998b8a828263d161ab745e6a3aec1907f
jongallant <jonbgallant@gmail.com>
2014-11-14 16:30:22 -0800

    Initial commit

---
+++ arduino/beakn.arduino.console/beakn.arduino.console/ArduinoSerial.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using CommandMessenger;
+using CommandMessenger.TransportLayer;
+using Microsoft.Lync.Model;
+
+namespace beakn.arduino.console
+{
+    enum Command
+    {
+        SetLed, // Command to request led to be set in specific state
+    };
+
+    public class ArduinoSerial
+    {
+        public bool RunLoop { get; set; }
+        private SerialTransport _serialTransport;
+        private CmdMessenger _cmdMessenger;
+
+        // Setup function
+        public void Setup()
+        {
+            // Create Serial Port object
+            _serialTransport = new SerialTransport();
+            _serialTransport.CurrentSerialSettings.PortName = "COM5";    // Set com port
+            _serialTransport.CurrentSerialSettings.BaudRate = 115200;     // Set baud rate
+            _serialTransport.CurrentSerialSettings.DtrEnable = false;     // For some boards (e.g. Sparkfun Pro Micro) DtrEnable may need to be true.
+            
+            // Initialize the command messenger with the Serial Port transport layer
+            _cmdMessenger = new CmdMessenger(_serialTransport);
+
+            // Tell CmdMessenger if it is communicating with a 16 or 32 bit Arduino board
+            _cmdMessenger.BoardType = BoardType.Bit16;
+            
+            // Attach the callbacks to the Command Messenger
+            AttachCommandCallBacks();
+            
+            // Start listening
+            _cmdMessenger.StartListening();                                
+        }
+
+        // Loop function
+        public void Loop()
+        {
+        }
+
+        public void Set(ContactAvailability avail)
+        {
+            _cmdMessenger.SendCommand(new SendCommand((int)Command.SetLed, avail.ToString()));
+        }
+
+        // Exit function
+        public void Exit()
+        {
+            // We will never exit the application
+        }
+
+        /// Attach command call backs. 
+        private void AttachCommandCallBacks()
+        {
+            // No callbacks are currently needed
+        }
+    }
+}
---
+++ arduino/beakn.arduino.console/beakn.arduino.console/Program.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Lync.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beakn.arduino.console
+{
+    class Program
+    {
+        private static LyncClient lyncClient;
+        private static ArduinoSerial arduinoSerial;
+
+        static void Main(string[] args)
+        {
+            //Listen for events of changes in the state of the client
+            try
+            {
+                lyncClient = LyncClient.GetClient();
+                arduinoSerial = new ArduinoSerial { RunLoop = true };
+                arduinoSerial.Setup();
+            }
+            catch (ClientNotFoundException clientNotFoundException)
+            {
+                Console.WriteLine(clientNotFoundException);
+                return;
+            }
+            catch (NotStartedByUserException notStartedByUserException)
+            {
+                Console.Out.WriteLine(notStartedByUserException);
+                return;
+            }
+            catch (LyncClientException lyncClientException)
+            {
+                Console.Out.WriteLine(lyncClientException);
+                return;
+            }
+            catch (SystemException systemException)
+            {
+                if (IsLyncException(systemException))
+                {
+                    // Log the exception thrown by the Lync Model API.
+                    Console.WriteLine("Error: " + systemException);
+                    return;
+                }
+                else
+                {
+                    // Rethrow the SystemException which did not come from the Lync Model API.
+                    throw;
+                }
+            }
+
+            lyncClient.StateChanged +=
+                new EventHandler<ClientStateChangedEventArgs>(Client_StateChanged);
+
+            lyncClient.Self.Contact.ContactInformationChanged +=
+                   new EventHandler<ContactInformationChangedEventArgs>(SelfContact_ContactInformationChanged);
+
+            SetAvailability();
+            Console.ReadKey();
+        }
+
+        private static void SelfContact_ContactInformationChanged(object sender, ContactInformationChangedEventArgs e)
+        {
+            //Only update the contact information in the user interface if the client is signed in.
+            //Ignore other states including transitions (e.g. signing in or out).
+            if (lyncClient.State == ClientState.SignedIn)
+            {
+                //Get from Lync only the contact information that changed.
+                if (e.ChangedContactInformation.Contains(ContactInformationType.Availability))
+                {
+                    //Use the current dispatcher to update the contact's availability in the user interface.
+                    SetAvailability();
+                }
+            }
+        }
+
+        private static void SetAvailability()
+        {
+            //Get the current availability value from Lync
+            ContactAvailability currentAvailability = 0;
+            try
+            {
+                currentAvailability = (ContactAvailability)lyncClient.Self.Contact.GetContactInformation(ContactInformationType.Availability);
+                arduinoSerial.Set(currentAvailability);
+            }
+            catch (LyncClientException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (SystemException systemException)
+            {
+                if (IsLyncException(systemException))
+                {
+                    // Log the exception thrown by the Lync Model API.
+                    Console.WriteLine("Error: " + systemException);
+                }
+                else
+                {
+                    // Rethrow the SystemException which did not come from the Lync Model API.
+                    throw;
+                }
+            }
+        }
+
+        private static void Client_StateChanged(object sender, ClientStateChangedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Identify if a particular SystemException is one of the exceptions which may be thrown
+        /// by the Lync Model API.
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        static private bool IsLyncException(SystemException ex)
+        {
+            return
+                ex is NotImplementedException ||
+                ex is ArgumentException ||
+                ex is NullReferenceException ||
+                ex is NotSupportedException ||
+                ex is ArgumentOutOfRangeException ||
+                ex is IndexOutOfRangeException ||
+                ex is InvalidOperationException ||
+                ex is TypeLoadException ||
+                ex is TypeInitializationException ||
+                ex is InvalidComObjectException ||
+                ex is InvalidCastException;
+        }
+    }
+}
